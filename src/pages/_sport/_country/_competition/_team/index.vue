71394d73e3ada9c97711aa11726cda91639a12a6
João Ferreira <jaferrreira88@gmail.com>
2018-03-20 19:15:14 +0000

    Table Top Scorers

--- src/pages/_sport/_country/_competition/_team/index.vue
+++ src/pages/_sport/_country/_competition/_team/index.vue
@@ -1,40 +1,75 @@
 <template>
   <div v-if="hasData == true">
-    <section class="container">
-        <div class="column is-8 is-offset-2">
-            <h1 class="title is-1">{{title}}</h1>
-            <p class="subtitle is-3">{{teamInfo.city}}</p>
-            <br/>
-            <h5>Manager: {{teamInfo.manager}}</h5>
-        </div>
-    </section>
-    <section>
-        <div class="column is-8 is-offset-2">
+      <div class="row">
+      <div class="column is-8 is-offset-2">
+        <progress class="progress is-success" value="60" max="100">60%</progress>
+        <div class="row">
+            <div class="column s-4">olaana</div>
+            <div class="column s-8">
+                <h1 class="title is-1">{{title}}</h1>
+                <p class="subtitle is-3">{{teamInfo.city}}</p>
+           </div>
+               <br/>
+                <h5>Manager: {{teamInfo.manager}}</h5>
+            </div>
+        
+ 
+        <section>
             <h4 class="title is-4 has-text-centered">Squad</h4>
-                   <table class="table is-fullwidth">
-                        <thead>
+            <div class="titlesLine"></div>
+                <table class="table is-fullwidth">
+                    <thead>
+                    <tr>
+                        <th>Number</th>
+                        <th>Name</th>
+                        <th>Position</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="player in squad" :key="player.name">
+                        <td >{{player.number}}</td>
+                        <td>{{player.name}}</td>
+                        <td>{{player.position}}</td>
+                    </tr>
+                    </tbody>
+                </table>
+        </section>
+        <section>
+            <h4 class="title is-4 has-text-centered">Top Scorers</h4>
+            <div class="titlesLine"></div>
+                <table class="table is-fullwidth">
+                    <thead>
                         <tr>
-                            <th>Number</th>
-                            <th>Name</th>
-                            <th>Position</th>
+                            <th>#</th>
+                            <th></th>
+                            <th></th>
                         </tr>
-                        </thead>
-                        <tbody>
-                        <tr v-for="player in squad" :key="player.name">
-                            <td >{{player.number}}</td>
+                    </thead>
+                    <tbody>
+                        <tr v-for="player in topScorers" :key="player.name">
+                            <td >{{player.position}}</td>
                             <td>{{player.name}}</td>
-                            <td>{{player.position}}</td>
+                            <td style="padding-bottom: 3px;"><strong style="font-size: 35px;">{{player.goals}}</strong> Goals </td>
                         </tr>
-                        </tbody>
-                    </table>
-            <ul>
+                    </tbody>
+                </table>
+        </section>
+
+    </div>
+      </div>
+
+
+  <hr />
+  <section>
+        <div>
+            <h4>Squad</h4>
+   <ul>
                 <li v-for="player in squad" :key="player.name">
                     {{player.number}} {{player.name}} [{{player.position}}]
                 </li>
             </ul>
         </div>
-    </section>
-
+  </section>
     <hr />
     <section>
         <div>
@@ -144,9 +179,12 @@ export default {
 </script>
 
 <style>
-body{
-    background-color: #f7f7f7;
+.titlesLine{
+    width: 100%;
+    height: 10px;
+    background-color: aquamarine;
+    margin-bottom: 30px;
+    margin-top: -10px !important;
 }
-
 
 </style>
