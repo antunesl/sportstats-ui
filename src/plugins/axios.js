482ee6ff664fe6d39897d0c2f2bb74100b0c4833
Luis Antunes <luis.antunes@wig.pt>
2018-03-21 16:55:30 +0000

    Change api base url.

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -8,7 +8,7 @@ if (process.server) {
   // if (!apiKeyValue)
   //   throw Error('ApiKey is not defined in configuration.');
   
-  options.baseURL = `http://wigserver.myvnc.com:3000/api/`;
+  options.baseURL = `http://server:3000/api/`;
   // options.headers = { 'api_key': apiKeyValue };
 }
 
