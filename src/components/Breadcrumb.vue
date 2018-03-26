5c80066b96e76e895150f13e6023102415e62a90
João Ferreira <jaferrreira88@gmail.com>
2018-03-26 19:26:08 +0100

    Breadcrumbs  Component

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -1,30 +1,29 @@
 <template>
 
   <nav class="breadcrumb">
-    <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/football">Sport</a></li>
-      <li class="is-active" v-for="country in this.$parent.countries" :key="country">
-        <a aria-current="page" v-bind:href="country.link">{{country.name}}</a>
+    <ul class="list-group">
+      <li class="list-group-item">
+        <a href="/">Home</a>
       </li>
-      <li class="is-active" v-for="competition in this.$parent.competitions" :key="competition.name">
-        <a aria-current="page" v-bind:href="competition.link">{{competition.name}}</a>
+      <li class="list-group-item">
+        <a href="/football">Sport</a>
       </li>
-      
-      <li class="is-active" v-for="team in this.$parent.teams" :key="team.name" >
-        <a aria-current="page">{{team.name}}</a>
+      <li class="list-group-item" >
+        <a href="#">Country</a>
+      </li>
+      <li class="list-group-item">
+        <a href="#" >Competition</a>
+      </li>
+      <li class="list-group-item active">
+        <a href="#">Team</a>
       </li>
     </ul>
   </nav>
-
 
 </template>
 
 
 <script>
-export default {
-
-  }
 
 </script>
 
